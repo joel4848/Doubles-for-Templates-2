c2e614862b9fc6a2b61fe59211e394eedacf7bc3
Joel Taylor <joel.w.taylor@gmail.com>
2025-11-09 14:30:58 +0000

    Initial commit

---
+++ src/client/java/com/joel4848/dft2/DFT2DataGenerator.java
@@ -0,0 +1,11 @@
+package com.joel4848.dft2;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DFT2DataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
